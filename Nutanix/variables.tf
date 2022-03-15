7a625fdb25306e78941f4e09903ea194d7474ae5
christianmarrero <christian.marrero@nutanix.com>
2022-03-15 14:20:22 -0400

    commit

---
+++ Nutanix/variables.tf
@@ -0,0 +1,24 @@
+variable  "nutanix_username" {
+    type = string
+}
+variable  "nutanix_password" {
+    type = string
+} 
+variable "nutanix_endpoint" {
+    type = string
+}
+variable "nutanix_network_name" {
+    type = string
+} 
+variable "nutanix_vlan_number" {
+    type = string
+}
+variable "nutanix_gateway_ip" {
+    type = string
+}
+variable "nutanix_subnet_ip" {
+    type = string
+}
+variable "nutanix_dns_server_name" {
+    type = string
+}
